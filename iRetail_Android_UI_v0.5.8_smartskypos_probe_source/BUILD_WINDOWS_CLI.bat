008c7e280129ad549635bb89efce2990273b1898
Andrey <microbook@mail.ru>
2026-09-20 02:23:43 +0300

    v0.5.34: align Windows build banner with S1 release

--- iRetail_Android_UI_v0.5.8_smartskypos_probe_source/BUILD_WINDOWS_CLI.bat
+++ iRetail_Android_UI_v0.5.8_smartskypos_probe_source/BUILD_WINDOWS_CLI.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
 
-rem i-Retail Android UI v0.5.8 SmartSkyPOS probe
+rem i-Retail Android UI v0.5.34 S1 truthful states
 rem Windows CLI debug APK build script.
 rem
 rem Important:
@@ -23,7 +23,7 @@ if errorlevel 1 (
     exit /b 9
 )
 
-set "SCRIPT_VERSION=0.5.8-smartskypos-probe"
+set "SCRIPT_VERSION=0.5.34-s1-truthful-states"
 set "DO_CLEAN=0"
 set "DO_HARD_CLEAN=0"
 
