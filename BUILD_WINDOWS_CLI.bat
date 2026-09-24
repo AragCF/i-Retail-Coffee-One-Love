510c0913cefcc8e3e7240d7820ee33f0d2619bd8
Andrey <microbook@mail.ru>
2026-09-24 15:04:28 +0300

    chore: sync Windows build to v0.5.114

--- BUILD_WINDOWS_CLI.bat
+++ BUILD_WINDOWS_CLI.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
 
-rem i-Retail Android UI v0.5.113 sbp callback contract
+rem i-Retail Android UI v0.5.114 sbp wire sanitizer
 rem Windows CLI debug APK build script.
 rem
 rem Important:
@@ -23,7 +23,7 @@ if errorlevel 1 (
     exit /b 9
 )
 
-set "SCRIPT_VERSION=0.5.113-sbp-callback-contract"
+set "SCRIPT_VERSION=0.5.114-sbp-wire-sanitizer"
 set "DO_CLEAN=0"
 set "DO_HARD_CLEAN=0"
 
