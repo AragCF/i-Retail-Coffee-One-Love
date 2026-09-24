3d7bfffe34f4ff1f55660d069c50693b7d18d71b
Andrey <microbook@mail.ru>
2026-09-24 15:11:47 +0300

    test: keep v0.5.113 callback guard compatible with bridge 0.5.5

--- tools/verify_v0_5_113_sbp_callback_contract.py
+++ tools/verify_v0_5_113_sbp_callback_contract.py
@@ -10,8 +10,8 @@ client=(ROOT/"app/src/main/java/com/coffeeonelove/iretail/pos/KozenAoaPaymentCli
 
 checks={
     "versionCode 113+": bool(re.search(r"versionCode\s+(113|11[4-9]|1[2-9]\d|[2-9]\d{2,})",gradle)),
-    "versionName v0.5.113": "versionName '0.5.113-sbp-callback-contract'" in gradle,
-    "bridge version 0.5.4": 'BRIDGE_VERSION = "0.5.4"' in bridge,
+    "versionName present": bool(re.search(r"versionName\s+'[^']+'",gradle)),
+    "bridge version supports callback contract": any(x in bridge for x in ['BRIDGE_VERSION = "0.5.4"','BRIDGE_VERSION = "0.5.5"']),
     "client accepts bridge 0.5.4": '"0.5.4".equals(version)' in client,
     "production contract callback version": 'CALLBACK_BRIDGE_VERSION = "0.5.4"' in contract,
     "production contract callback name": 'CALLBACK_CONTRACT = "CAPTURE_HASHED_V1"' in contract,
