7232c2979c06cee18f89be5c14ea3e9d96b6718b
Andrey <microbook@mail.ru>
2026-09-25 02:26:42 +0300

    v0.5.119: bump Windows build version

--- BUILD_WINDOWS_CLI.bat
+++ BUILD_WINDOWS_CLI.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
 
-rem i-Retail Android UI v0.5.118 SBP Android 6 restore fix
+rem i-Retail Android UI v0.5.119 SBP landscape QR
 rem Windows CLI debug APK build script.
 rem
 rem Important:
@@ -23,7 +23,7 @@ if errorlevel 1 (
     exit /b 9
 )
 
-set "SCRIPT_VERSION=0.5.118-sbp-android6-restore-fix"
+set "SCRIPT_VERSION=0.5.119-sbp-landscape-qr"
 set "DO_CLEAN=0"
 set "DO_HARD_CLEAN=0"
 
